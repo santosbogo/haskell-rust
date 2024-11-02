46d871227e96b127059719c7b782b4a7f2156e70
Santos Bogo <72747461+santosbogo@users.noreply.github.com>
2024-11-02 11:40:55 -0300

    collatz implemented and tested

--- rust-exercises/collatz-conjecture/src/lib.rs
+++ rust-exercises/collatz-conjecture/src/lib.rs
@@ -1,4 +1,19 @@
 pub fn collatz(mut n: u64) -> Option<u64> {
-  None
+  if n == 0 {
+    return None;
+  }
+
+  let mut steps = 0;
+
+  while n != 1 {
+    n = if n % 2 == 0 {
+      n/2
+    } else {
+      3 * n + 1
+    };
+    steps += 1;
+  }
+
+  Some(steps)
 }
 
