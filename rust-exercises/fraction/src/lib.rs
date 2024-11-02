1824657a2a07ab1142f625ada9b3a84d785cd43c
Santos Bogo <72747461+santosbogo@users.noreply.github.com>
2024-11-02 13:01:03 -0300

    Fraction operations now simplifying result

--- rust-exercises/fraction/src/lib.rs
+++ rust-exercises/fraction/src/lib.rs
@@ -4,28 +4,28 @@ type Fraction = (i32, i32);
 pub fn add((n1, d1): Fraction, (n2, d2): Fraction) -> Fraction {
     let numerator = n1 * d2 + n2 * d1;
     let denominator = d1 * d2;
-    (numerator, denominator)
+    simplify(numerator, denominator)
 }
 
 /// Subtract 2 fractions
 pub fn sub((n1, d1): Fraction, (n2, d2): Fraction) -> Fraction {
     let numerator = n1 * d2 - n2 * d1;
     let denominator = d1 * d2;
-    (numerator, denominator)
+    simplify(numerator, denominator)
 }
 
 /// Multiply 2 fractions
 pub fn mul((n1, d1): Fraction, (n2, d2): Fraction) -> Fraction {
     let numerator = n1 * n2;
     let denominator = d1 * d2;
-    (numerator, denominator)
+    simplify(numerator, denominator)
 }
 
 /// Divide 2 fractions
 pub fn divide((n1, d1): Fraction, (n2, d2): Fraction) -> Fraction {
     let numerator = n1 * d2;
     let denominator = d1 * n2;
-    (numerator, denominator)
+    simplify(numerator, denominator)
 }
 
 /// Calculate the Highest common factor between 2 numbers
@@ -38,13 +38,13 @@ pub fn hcf(a: i32, b: i32) -> i32 {
 
 /// Create a fraction simplifying with the arguments simplified by the `hcf`
 pub fn simplify(n: i32, d: i32) -> Fraction {
-    (n, d)
-    // let divisor = hcf(n, d);
-    // let (num, den) = (n / divisor, d / divisor);
-    //
-    // if den < 0 {
-    //     (-num, -den)
-    // } else {
-    //     (num, den)
-    // }
+    // (n, d)
+    let divisor = hcf(n, d);
+    let (num, den) = (n / divisor, d / divisor);
+
+    if den < 0 {
+        (-num, -den)
+    } else {
+        (num, den)
+    }
 }
