b18706196b0bd8e2c51049dc4d25df3462d182f2
Santos Bogo <72747461+santosbogo@users.noreply.github.com>
2024-11-18 00:53:29 -0300

    Collatz Conjecture practice

--- haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,19 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz a
+    | a <= 0 = Nothing
+    | otherwise = Just $ toInteger $ (length $ collatzListBuilder a) - 1
+
+collatzListBuilder:: Integer -> [Integer]
+collatzListBuilder a
+    | a == 1 = [1]
+    | a `mod` 2 == 0 = a : collatzListBuilder(a `div` 2)
+    | otherwise = a : collatzListBuilder(3 * a + 1)
+
+--collatz :: Integer -> Maybe Integer
+--collatz a
+--    | a <= 0 = Nothing
+--    | a == 1 = Just 0
+--    | a `mod` 2 == 0 = fmap(+1) $ collatz(a `div` 2)
+--    | otherwise = fmap(+1) $ collatz(3 * a + 1)
