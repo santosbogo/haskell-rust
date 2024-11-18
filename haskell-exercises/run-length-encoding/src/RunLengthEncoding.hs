c001c6530b19942904b5760367423f2d6ff7a55e
Santos Bogo <72747461+santosbogo@users.noreply.github.com>
2024-11-18 19:14:02 -0300

    More understandable but bigger

--- haskell-exercises/run-length-encoding/src/RunLengthEncoding.hs
+++ haskell-exercises/run-length-encoding/src/RunLengthEncoding.hs
@@ -1,10 +1,20 @@
 module RunLengthEncoding (encode, encode2, Element(..)) where
 
+--encode :: String -> [(Int, Char)]
+--encode [] = []
+--encode (c:word) =
+--    let (prefix, rest) = span (== c) word
+--    in (1 + length prefix, c)  : encode rest
+
 encode :: String -> [(Int, Char)]
 encode [] = []
-encode (c:word) =
-    let (prefix, rest) = span (== c) word
-    in (1 + length prefix, c)  : encode rest
+encode (x : xs) = encode' 1 x xs
+
+encode' :: Int -> Char -> String -> [(Int, Char)]
+encode' n c "" = [(n, c)]
+encode' n c (x : xs)
+    | c == x = encode' (n+1) c xs
+    | otherwise = (n, c) : encode' 1 x xs
 
 
 data Element =  One  Char | Repeat Char Int deriving(Show, Eq)
